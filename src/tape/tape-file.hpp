1916c31e423fb29cdb1660c18213c1a528f37789
flown4qqqq <igorkoshkarev4qqq@mail.ru>
2024-10-12 20:33:38 +0300

    type of Move* changed from "void" to "bool"

--- src/tape/tape-file.hpp
+++ src/tape/tape-file.hpp
@@ -1,15 +1,25 @@
+#include <string>
 #include "tape-interface.hpp"
 
 namespace NTape {
 
 class TTapeFile final : ITape {
 private:
+    uint32_t pos;
+    size_t size;
+    FILE* file;
+
+    static const size_t normalizeCoefficient = 19;
+    static std::string normilize(uint32_t);
+
     ~TTapeFile();
-    TTapeFile();
 public:
+    TTapeFile();
+    TTapeFile(std::string);
+
     void Delete() override;
-    void MoveLeft() override;
-    void MoveRight() override;
+    bool MoveLeft() override;
+    bool MoveRight() override;
     uint32_t ReadUInt32() const override;
     void WriteUInt32(uint32_t) override;
 };
--- src/tape/tape-interface.hpp
+++ src/tape/tape-interface.hpp
@@ -7,8 +7,8 @@ class ITape : NBaseInterfaces::IBase {
 protected:
     virtual ~ITape() = default;
 public:
-    virtual void MoveLeft() = 0;
-    virtual void MoveRight() = 0;
+    virtual bool MoveLeft() = 0;
+    virtual bool MoveRight() = 0;
     virtual uint32_t ReadUInt32() const = 0;
     virtual void WriteUInt32(uint32_t) = 0;
 };

f3ff1f8ffdcf9068bd2c2f2b5afc4e2c2779b4c0
flown4qqqq <igorkoshkarev4qqq@mail.ru>
2024-10-14 16:32:31 +0300

    Add ut for sorting

--- ut/tape_ut.cpp
+++ ut/tape_ut.cpp
@@ -4,20 +4,40 @@
 #include <unistd.h>
 #include <filesystem>
 #include <fstream>
+#include <random>
+#include <ctime>
 
 #include <tape-file.hpp>
+
+namespace {
+
+template <class I>
+using TUniquePtr = std::unique_ptr<I, NBaseInterfaces::IBase::BaseDeleter>;
+
+
 
 std::string GetCurrentDirectory() {
     return get_current_dir_name();
 }
 
-template <class I>
-using TUniquePtr = std::unique_ptr<I, NBaseInterfaces::IBase::BaseDeleter>;
+std::string PathDir;
+std::mt19937 RandomGenerator(static_cast<uint32_t>(std::time(nullptr)));
+
+std::string GenerateRandomPath() {
+    std::string path = PathDir + "/";
+
+    for (size_t i = 0; i < 20; i++) {
+        char c = 'a' + RandomGenerator() % ('z' - 'a' + 1);
+        path += c;
+    }
+
+    path += ".txt";
+    return path;
+}
+
 
 TEST(TapeFileUt, CreateReadWrite) {
-    std::string pathDir = GetCurrentDirectory() + "/tmp";
-    std::filesystem::create_directory(pathDir);
-    std::string pathFile = pathDir + "/a.txt";
+    std::string pathFile = GenerateRandomPath();
     std::ofstream out;
     out.open(pathFile);
     out << "10 20 30" << std::endl;
@@ -40,15 +60,10 @@ TEST(TapeFileUt, CreateReadWrite) {
     actual.push_back(tape->ReadUInt32());
 
     ASSERT_EQ(expected, actual);
-
-    // todo smth with it
-    std::filesystem::remove_all(pathDir);
 }
 
 TEST(TapeFileUt, SetGetCurrentPosition) {
-    std::string pathDir = GetCurrentDirectory() + "/tmp";
-    std::filesystem::create_directory(pathDir);
-    std::string pathFile = pathDir + "/a.txt";
+    std::string pathFile = GenerateRandomPath();
     std::ofstream out;
     out.open(pathFile);
     out << "10 20 30" << std::endl;
@@ -64,14 +79,10 @@ TEST(TapeFileUt, SetGetCurrentPosition) {
     ASSERT_EQ(tape->ReadUInt32(), static_cast<uint32_t>(10));
     tape->SetCurrentPosition(2);
     ASSERT_EQ(tape->ReadUInt32(), static_cast<uint32_t>(30));
-
-    std::filesystem::remove_all(pathDir);
 }
 
 TEST(TapeFileUt, WriteMoveForce) {
-    std::string pathDir = GetCurrentDirectory() + "/tmp";
-    std::filesystem::create_directory(pathDir);
-    std::string pathFile = pathDir + "/a.txt";
+    std::string pathFile = GenerateRandomPath();
     std::ofstream out;
     out.open(pathFile);
 
@@ -100,11 +111,73 @@ TEST(TapeFileUt, WriteMoveForce) {
     tape->MoveRight();
     actual.push_back(tape->ReadUInt32());
     tape->MoveRight();
+}
 
-    std::filesystem::remove_all(pathDir);
+TEST(TapeSort, SortSmall) {
+    std::string inFile = GenerateRandomPath();
+    std::ofstream in;
+    in.open(inFile);
+    in << "16 5 2 1 3 2 17" << std::endl;
+    in.close();
+
+    std::string outFile = GenerateRandomPath();
+
+    NTape::Sort(inFile, outFile, 1024);
+
+    std::vector<uint32_t> expected = {1, 2, 2, 3, 5, 16, 17};
+    std::vector<uint32_t> actual = {};
+
+    std::ifstream out;
+    out.open(outFile);
+
+    uint32_t x;
+    while (out >> x) {
+        actual.push_back(x);
+    }
+
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(TapeSort, SortWithNotDefaultOption) {
+    std::string inFile = GenerateRandomPath();
+    std::ofstream in;
+    in.open(inFile);
+    in << "16 5 2 1 3 2 17 0 1 3 4" << std::endl;
+    in.close();
+
+    std::string outFile = GenerateRandomPath();
+
+    NTape::Sort(inFile, outFile, 4);
+
+    std::vector<uint32_t> expected = {0, 1, 1, 2, 2, 3, 3, 4, 5, 16, 17};
+    std::vector<uint32_t> actual = {};
+
+    std::ifstream out;
+    out.open(outFile);
+
+    uint32_t x;
+    while (out >> x) {
+        actual.push_back(x);
+    }
+
+    ASSERT_EQ(expected, actual);
+}
+
+}; // namespace
+
+void Prepare() {
+    PathDir = GetCurrentDirectory() + "/tmp";
+    std::filesystem::create_directory(PathDir);
+}
+
+void Complete() {
+    std::filesystem::remove_all(PathDir);
 }
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    Prepare();
+    auto result = RUN_ALL_TESTS();
+    Complete();
+    return result;
 }
